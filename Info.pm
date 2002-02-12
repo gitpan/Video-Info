11e05e83d1d2a822ed2b1e039d641e029bc46bbc
Allen Day <allenday@cpan.org>
2002-02-12 17:37:48 +0000

    Import of ALLENDAY/Video-Info-0.06 from CPAN.
    gitpan-cpan-distribution: Video-Info
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         ALLENDAY/Video-Info-0.06.tar.gz
    gitpan-cpan-author:       ALLENDAY
    gitpan-cpan-maturity:     released

--- Info.pm
+++ Info.pm
@@ -65,7 +65,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw( );
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 for my $datum ( keys %FIELDS ) {
     no strict "refs"; ## to register new methods in package
